fbccb37c2bd993432c072bd2dc036e9352209e67
Weronika Żochowiec <Veronica.zochowiec@gmail.com>
2021-03-08 01:20:48 +0100

    format code

--- rail_fence.py
+++ rail_fence.py
@@ -3,7 +3,7 @@ def encrypt_message_rail_fence(message: str, key: int) -> str:
     position: int = 0
     change: int = 1
 
-    message_table = [['*' for i in range(0, len(message))] for j in range(0, key)]
+    message_table = [["*" for i in range(0, len(message))] for j in range(0, key)]
 
     for i in range(len(message)):
         message_table[position][i] = message[i]
@@ -13,11 +13,11 @@ def encrypt_message_rail_fence(message: str, key: int) -> str:
             continue
         else:
             change *= -1
-            position += change*2
+            position += change * 2
 
     for i in range(key):
         for j in range(len(message)):
-            if not message_table[i][j] == '*':
+            if not message_table[i][j] == "*":
                 encrypted_message += message_table[i][j]
 
     return encrypted_message
@@ -30,35 +30,34 @@ def decrypt_message_rail_fence(encrypted_message: str, key: int) -> str:
     col: int = 0
     x: int = 0
 
-    message_table = [['*' for i in range(0, len(encrypted_message))] for j in range(0, key)]
+    message_table = [
+        ["*" for i in range(0, len(encrypted_message))] for j in range(0, key)
+    ]
 
     for i in range(len(encrypted_message)):
-        message_table[row][col] = '#'
-        if row == 0 or row == (key -1):
-            k*= -1
+        message_table[row][col] = "#"
+        if row == 0 or row == (key - 1):
+            k *= -1
 
         row += k
         col += 1
 
-
     for i in range(key):
         for j in range(len(encrypted_message)):
-            if message_table[i][j] == '#':
+            if message_table[i][j] == "#":
                 message_table[i][j] = encrypted_message[x]
-                x+=1
+                x += 1
 
     row = 0
     col = 0
     k = -1
 
-
     for i in range(len(encrypted_message)):
         decrypted_message += message_table[row][col]
-        if row == 0 or row == (key -1):
-            k*= -1
+        if row == 0 or row == (key - 1):
+            k *= -1
 
         row += k
         col += 1
-
 
     return decrypted_message
--- view.py
+++ view.py
@@ -2,8 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from rail_fence import decrypt_message_rail_fence, encrypt_message_rail_fence
-from columnar_transposition import decrypt_columnar_transposition_b, encrypt_columnar_transposition_b, \
-    decrypt_columnar_transposition_a, encrypt_columnar_transposition_a
+from columnar_transposition import (
+    decrypt_columnar_transposition_b,
+    encrypt_columnar_transposition_b,
+    decrypt_columnar_transposition_a,
+    encrypt_columnar_transposition_a,
+)
 
 
 NORM_FONT = ("Verdana", 10)
@@ -26,21 +30,30 @@ class BskApp(tk.Tk):
 class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.top_frame = tk.Frame(self, bg='gray22', width=600, height=100, pady=5, padx=5)
+        self.top_frame = tk.Frame(
+            self, bg="gray22", width=600, height=100, pady=5, padx=5
+        )
         self.center_frame = CenterFrame(self, controller)
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.top_frame.grid(row=0, sticky="ew")
         self.center_frame.grid(row=1, sticky="nsew")
 
-        self.logo = tk.Label(self.top_frame, text="BSK APP", bg='gray22',
-                             fg='gray63', width="100", height="2", font=("Verdana", 20, 'bold')).pack()
+        self.logo = tk.Label(
+            self.top_frame,
+            text="BSK APP",
+            bg="gray22",
+            fg="gray63",
+            width="100",
+            height="2",
+            font=("Verdana", 20, "bold"),
+        ).pack()
 
 
 class CenterFrame(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.config(bg='gray5', width=1280, height=620)
+        self.config(bg="gray5", width=1280, height=620)
         self.menu = Menu(self)
         self.user_data = RightFrame(self, controller)
         self.data = None
@@ -56,7 +69,7 @@ class CenterFrame(tk.Frame):
         if self.data is not None:
             self.data.destroy()
         self.data = new_frame
-        self.data.grid(row=0, column=1, sticky='nsew')
+        self.data.grid(row=0, column=1, sticky="nsew")
 
 
 class DataFrame(tk.Frame):
@@ -68,93 +81,129 @@ class DataFrame(tk.Frame):
 class RightFrame(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.config(bg='gray31', width=50, height=620, padx=3, pady=3)
+        self.config(bg="gray31", width=50, height=620, padx=3, pady=3)
 
 
 class Menu(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.config(bg='gray30', width=200, height=620, padx=3)
-        button1 = tk.Button(self, text="Implementacja podstawowych modułów kryptograficznych", fg='gray68', bg='gray14',
-                            font=('calibri', 10, 'bold'), height=3,
-                                            command=lambda: parent.switch_frame(BasicCryptographyFrame))
+        self.config(bg="gray30", width=200, height=620, padx=3)
+        button1 = tk.Button(
+            self,
+            text="Implementacja podstawowych modułów kryptograficznych",
+            fg="gray68",
+            bg="gray14",
+            font=("calibri", 10, "bold"),
+            height=3,
+            command=lambda: parent.switch_frame(BasicCryptographyFrame),
+        )
         button1.grid(column=0, row=1, pady=3)
 
 
 class BasicCryptographyFrame(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.config(width=880, height=620, padx=3, pady=3, bg='gray70')
+        self.config(width=880, height=620, padx=3, pady=3, bg="gray70")
         self.option_frame = tk.Frame(self)
         self.data_frame = tk.Frame(self)
-        self.option_frame.config(width=300, height=620, padx=3, pady=3, bg='gray70')
-        self.data_frame.config(width=580, height=620, padx=3, pady=3, bg='snow')
+        self.option_frame.config(width=300, height=620, padx=3, pady=3, bg="gray70")
+        self.data_frame.config(width=580, height=620, padx=3, pady=3, bg="snow")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         self.option_frame.grid(row=0, column=1, sticky="nsew")
-        self.data_frame.grid(row=0, column=0, sticky='nsew')
-        self.option_label = tk.Label(self.option_frame, text="OPCJE", bg='gray70',
-                             fg='gray21', font=("calibri", 11, 'bold')).pack()
-        sep = tk.Frame(self.option_frame, width=1, bg='gray30')
-        sep.pack(fill='x')
+        self.data_frame.grid(row=0, column=0, sticky="nsew")
+        self.option_label = tk.Label(
+            self.option_frame,
+            text="OPCJE",
+            bg="gray70",
+            fg="gray21",
+            font=("calibri", 11, "bold"),
+        ).pack()
+        sep = tk.Frame(self.option_frame, width=1, bg="gray30")
+        sep.pack(fill="x")
 
         self.coded_or_decoded = tk.IntVar()
         self.coded_or_decoded.set(1)
 
-        options = [("Szyfrowanie", 101),
-                     ("Deszyfrowanie", 102)]
+        options = [("Szyfrowanie", 101), ("Deszyfrowanie", 102)]
 
         def ShowChoice():
             print(self.coded_or_decoded.get())
 
-
         for option, val in options:
-            tk.Radiobutton(self.option_frame,
-                           text=option,
-                           padx=20,
-                           variable=self.coded_or_decoded,
-                           command=ShowChoice,
-                           value=val, bg="gray70").pack(anchor=tk.W)
+            tk.Radiobutton(
+                self.option_frame,
+                text=option,
+                padx=20,
+                variable=self.coded_or_decoded,
+                command=ShowChoice,
+                value=val,
+                bg="gray70",
+            ).pack(anchor=tk.W)
 
-        sep = tk.Frame(self.option_frame, width=1, bg='gray30')
-        sep.pack(fill='x')
+        sep = tk.Frame(self.option_frame, width=1, bg="gray30")
+        sep.pack(fill="x")
 
+        ciphers = [
+            "Rail Fence",
+            "Przestawienie macierzowe A",
+            "Przestawienie macierzowe B",
+        ]
 
-        ciphers = ["Rail Fence", "Przestawienie macierzowe A", "Przestawienie macierzowe B"]
-
-        self.cipher_label = tk.Label(self.option_frame, text="Wybierz metode szyfrowania: ", bg='gray70',
-                                     fg='gray16').pack()
+        self.cipher_label = tk.Label(
+            self.option_frame,
+            text="Wybierz metode szyfrowania: ",
+            bg="gray70",
+            fg="gray16",
+        ).pack()
         self.cipher = tk.StringVar()
-        cipher_choosen = ttk.Combobox(self.option_frame, textvariable=self.cipher, state="readonly", width=25)
-        cipher_choosen['values'] = ciphers
+        cipher_choosen = ttk.Combobox(
+            self.option_frame, textvariable=self.cipher, state="readonly", width=25
+        )
+        cipher_choosen["values"] = ciphers
         cipher_choosen.pack(pady=10)
         cipher_choosen.current(0)
-        open_file_button = tk.Button(self.option_frame, text="wczytaj plik tekstowy",
-                                     width=20, command=lambda: self.open_file()).pack()
-        save_file_button = tk.Button(self.option_frame, text="zapisz jako plik tekstowy",
-                                     width=20, bg='grey21', fg='snow', command=lambda: self.save_file()).pack()
+        open_file_button = tk.Button(
+            self.option_frame,
+            text="wczytaj plik tekstowy",
+            width=20,
+            command=lambda: self.open_file(),
+        ).pack()
+        save_file_button = tk.Button(
+            self.option_frame,
+            text="zapisz jako plik tekstowy",
+            width=20,
+            bg="grey21",
+            fg="snow",
+            command=lambda: self.save_file(),
+        ).pack()
 
         l_key = tk.Label(self.data_frame, text="Wprowadz klucz:")
         self.key = tk.Text(self.data_frame, width=30, height=1, wrap=tk.WORD)
         l1 = tk.Label(self.data_frame, text="Wprowadź tekst:")
         self.entry = tk.Text(self.data_frame, width=85, height=3, wrap=tk.WORD)
-        button = tk.Button(self.data_frame, text="Szyfruj / Deszyfruj ", width=20, command=lambda: self.decrypt_encrypt())
+        button = tk.Button(
+            self.data_frame,
+            text="Szyfruj / Deszyfruj ",
+            width=20,
+            command=lambda: self.decrypt_encrypt(),
+        )
         l2 = tk.Label(self.data_frame, text="Zaszyfrowany/ Zdeszyfrowany tekst:")
         self.output = tk.Text(self.data_frame, width=85, height=3, wrap=tk.WORD)
 
-
         l_key.grid(row=0, column=1, padx=5, sticky="w")
-        self.key.grid(row=1, column=1, padx=5, pady=(0, 10), sticky='w' )
+        self.key.grid(row=1, column=1, padx=5, pady=(0, 10), sticky="w")
         l1.grid(row=2, column=1, padx=5, sticky="w")
         self.entry.grid(row=3, column=1, columnspan=2, padx=5, pady=(0, 10))
         button.grid(row=4, column=1, columnspan=2, pady=5)
         l2.grid(row=5, column=1, padx=5, sticky="w")
         self.output.grid(row=6, column=1, columnspan=2, padx=5, pady=(0, 10))
 
-
     def open_file(self):
-        filename = fd.askopenfilename(filetypes=[("Plik tekstowy", "*.txt")])  # wywołanie okna dialogowego open file
+        filename = fd.askopenfilename(
+            filetypes=[("Plik tekstowy", "*.txt")]
+        )  # wywołanie okna dialogowego open file
 
         if filename:
             with open(filename, "r", -1, "utf-8") as file:
@@ -162,8 +211,9 @@ class BasicCryptographyFrame(tk.Frame):
                 self.entry.insert(tk.END, file.read())
 
     def save_file(self):
-        filename = fd.asksaveasfilename(filetypes=[("Plik tekstowy", "*.txt")],
-                                        defaultextension="*.txt")  # wywołanie okna dialogowego save file
+        filename = fd.asksaveasfilename(
+            filetypes=[("Plik tekstowy", "*.txt")], defaultextension="*.txt"
+        )  # wywołanie okna dialogowego save file
 
         if filename:
             with open(filename, "w", -1, "utf-8") as file:
@@ -177,31 +227,30 @@ class BasicCryptographyFrame(tk.Frame):
         result = ""
 
         if decrypt_or_encrypt == 101:
-            if cipher.upper()=="RAIL FENCE":
+            if cipher.upper() == "RAIL FENCE":
                 if self.rail_fence_validation(key):
                     key = int(key)
                     result = encrypt_message_rail_fence(message, key)
-            if cipher.upper()=="PRZESTAWIENIE MACIERZOWE A":
+            if cipher.upper() == "PRZESTAWIENIE MACIERZOWE A":
                 if self.columnar_transposition_validation_A(key):
                     result = encrypt_columnar_transposition_a(message, key)
-            if cipher.upper()=="PRZESTAWIENIE MACIERZOWE B":
+            if cipher.upper() == "PRZESTAWIENIE MACIERZOWE B":
                 if self.columnar_transposition_validation_B(key):
 
                     result = encrypt_columnar_transposition_b(message, key)
         elif decrypt_or_encrypt == 102:
-            if cipher.upper()=="RAIL FENCE":
+            if cipher.upper() == "RAIL FENCE":
                 if self.rail_fence_validation(key):
                     key = int(key)
                     result = decrypt_message_rail_fence(message, key)
-            if cipher.upper()=="PRZESTAWIENIE MACIERZOWE A":
+            if cipher.upper() == "PRZESTAWIENIE MACIERZOWE A":
                 if self.columnar_transposition_validation_A(key):
                     result = decrypt_columnar_transposition_a(message, key)
-            if cipher.upper()=="PRZESTAWIENIE MACIERZOWE B":
+            if cipher.upper() == "PRZESTAWIENIE MACIERZOWE B":
                 if self.columnar_transposition_validation_B(key):
                     result = decrypt_columnar_transposition_b(message, key)
 
         self.output.insert(tk.END, str(result))
-
 
     def rail_fence_validation(self, key):
         if not is_integer(key):
@@ -219,7 +268,6 @@ class BasicCryptographyFrame(tk.Frame):
         else:
             popupmsg("Klucz musi składać się tylko z liter alfabetu")
             return False
-
 
     def columnar_transposition_validation_A(self, key):
         if not check_key_format_col_trans_A(key):
@@ -244,12 +292,12 @@ def check_key_format_col_trans_A(key):
     check = 0
     for i in range(len(key)):
         if is_integer(key[i]):
-            check+=1
-        if key[i] == '-' and i!=0:
-            if is_integer(key[i-1]) and i == len(key):
-                check+=1
-            elif is_integer(key[i-1]) and is_integer(key[i+1]):
-                check+=1
+            check += 1
+        if key[i] == "-" and i != 0:
+            if is_integer(key[i - 1]) and i == len(key):
+                check += 1
+            elif is_integer(key[i - 1]) and is_integer(key[i + 1]):
+                check += 1
 
     if check == len(key):
         return True
@@ -258,8 +306,7 @@ def check_key_format_col_trans_A(key):
 
 
 def checkConsecutive(l):
-    return sorted(l) == list(range(1, len(l)+1))
-
+    return sorted(l) == list(range(1, len(l) + 1))
 
 
 def check_numbers_sequence(key):
@@ -267,7 +314,7 @@ def check_numbers_sequence(key):
     number = ""
     for i in key:
         if i != "-":
-            number+=i
+            number += i
         else:
             key_numbers_list.append((int(number)))
             number = ""
@@ -280,7 +327,6 @@ def check_numbers_sequence(key):
         return False
 
 
-
 def popupmsg(msg):
     popup = tk.Tk()
     popup.geometry("400x100")
@@ -290,5 +336,3 @@ def popupmsg(msg):
     B1 = tk.Button(popup, text="OK", command=popup.destroy)
     B1.pack()
     popup.mainloop()
-
-
